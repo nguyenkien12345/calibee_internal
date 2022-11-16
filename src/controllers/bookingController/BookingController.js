fe17afc94b79a5367215bcb14afd98d146250e79
nhphu19 <nhphu19@gmail.com>
2022-11-16 17:51:46 +0700

    update booking from app to Zoho --continue-03

--- src/controllers/bookingController/BookingController.js
+++ src/controllers/bookingController/BookingController.js
@@ -266,13 +266,89 @@ const BookingController = {
                 return res.json(onBuildResponseErr('error_not_found_booking'));
             }
 
-            console.log('booking', booking.booking_id);
+            let booking_detail = await BookingCommon.onGetBookingDetailByBookingID(booking.booking_id);
+            if (!booking_detail) {
+                return res.json(onBuildResponseErr('error_not_found_booking_detail'));
+            }
+
+            let status_booking = booking_detail.status;
+            let app_ids = booking_detail.app_ids;
+            let zoho_ids = booking_detail.booking_id_crm;
+
+            Order_ID = !Order_ID ? booking.booking_id_crm : Order_ID;
+            if (Status) {
+                switch (Status) {
+                    case 'Draft': {
+                        status_booking = 0;
+                        break;
+                    }
+                    case 'Confirmed': {
+                        status_booking = 1;
+                        break;
+                    }
+                    case 'Received': {
+                        status_booking = 2;
+                        break;
+                    }
+                    case 'Processing': {
+                        status_booking = 3;
+                        break;
+                    }
+                    case 'Completed': {
+                        status_booking = 4;
+                        break;
+                    }
+                    case 'Canceled': {
+                        status_booking = 5;
+                        break;
+                    }
+                }
+            }
+            console.log('Booking_ID', Booking_ID);
+            if (Booking_ID) {
+                app_ids = JSON.stringify(Object.keys(Booking_ID));
+                zoho_ids = JSON.stringify(Object.values(Booking_ID));
+                console.log('app_ids', app_ids);
+            }
+
+            // Save info booking
+            booking.booking_id_crm = Order_ID;
+            await booking.save();
+            booking_detail.app_ids = app_ids;
+            booking_detail.booking_id_crm = zoho_ids;
+            booking_detail.status = status_booking;
+            await booking_detail.save();
+
+            return res.status(200).json({
+                ...successCallBack,
+                data: {
+                    success: true,
+                    Booking_ID,
+                    booking,
+                    booking_detail,
+                },
+            });
+        } catch (err) {
+            next(err);
+        }
+    },
+
+    // Update info booking to Zoho
+    updateInfoBookingFromCRM: async (req, res, next) => {
+        try {
+            let { Sale_Order_ID, Status, Booking_ID } = req.body;
+
+            if (!Sale_Order_ID) return res.status(400).json(error_missing_params('Sale_Order_ID'));
+
+            let booking = await BookingCommon.onGetBookingByID_CRM(Sale_Order_ID);
+            if (!booking) {
+                return res.json(onBuildResponseErr('error_not_found_booking'));
+            }
 
             let booking_detail = await BookingCommon.onGetBookingDetailByBookingID(booking.booking_id);
             if (!booking_detail) {
                 return res.json(onBuildResponseErr('error_not_found_booking_detail'));
             }
-            console.log('booking_detail', booking_detail.booking_id);
 
             let status_booking = booking_detail.status;
             let app_ids = booking_detail.app_ids;
