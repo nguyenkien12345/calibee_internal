89f625691df0a533416a3aa99d2474637f5f0f09
nhphu19 <nhphu19@gmail.com>
2022-11-22 00:21:53 +0700

    tmp sync with zoho

--- src/routes/booking/BookingRoute.js
+++ src/routes/booking/BookingRoute.js
@@ -2,6 +2,7 @@ const BookingRouter = require('express').Router();
 const BookingController = require('../../controllers/bookingController/BookingController');
 const security = require('../../middleware/security');
 
+BookingRouter.put('/update-info-booking-crm', security.verifySecurity, BookingController.updateInfoBookingFromCRM);
 BookingRouter.post('/update-info-booking-to-crm', security.verifySecurity, BookingController.updateInfoBookingToCRM);
 BookingRouter.put('/assign-worker-booking-from-crm', security.verifySecurity, BookingController.assignWorkerFromCRM);
 BookingRouter.post('/completed-shift-from-crm', security.verifySecurity, BookingController.completedShiftFromCRM);
