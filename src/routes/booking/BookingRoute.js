78108ef2c237daa2f5c1faae91efab0e5ed1312e
khanhduong2t2 <khanhduong2t2@gmail.com>
2023-02-09 17:23:07 +0700

    [Duong] Update route

--- src/routes/booking/BookingRoute.js
+++ src/routes/booking/BookingRoute.js
@@ -7,4 +7,6 @@ BookingRouter.post('/update-info-booking-to-crm', security.verifySecurity, Booki
 BookingRouter.get('/booking-from-crm', security.verifySecurity, BookingController.getBookingFromCRM);
 BookingRouter.post('/create-contract', security.verifySecurity, BookingController.createContract);
 
+CustomerRouter.post('/create-job-zoho', security.verifySecurity, BookingController.createJobZoho);
+
 module.exports = BookingRouter;
