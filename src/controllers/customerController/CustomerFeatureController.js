172a5266f6f243aa8d5ed9bfe503e7dfc03ca1bc
khanhduong2t2 <khanhduong2t2@gmail.com>
2023-02-13 10:02:09 +0700

    [Duong] Write API updateBookingCRM

--- src/controllers/customerController/CustomerFeatureController.js
+++ src/controllers/customerController/CustomerFeatureController.js
@@ -231,6 +231,110 @@ const CustomerFeatureController = {
 			next(err);
 		}
 	},
+
+	updateBookingZoho: async (req, res, next) => {
+		try {
+			let {
+				App_Id_Booking,
+				Booking_ID,
+				Customer_Name,
+				Location,
+				Booking_Status,
+				Payment_Status,
+				Start_Date,
+				End_Date,
+				Sub_Total,
+				Discount,
+				Tip,
+				Total,
+				Booking_Payable_to_Worker,
+				Worker_Information,
+			} = req.body;
+			let { env } = req.query;
+			if (!env) return res.status(400).json(error_missing_params('env'));
+			if (!App_Id_Booking) return res.status(400).json(error_missing_params('App_Id_Attendance'));
+			if (!Booking_ID) return res.status(400).json(error_missing_params('Booking_ID'));
+			if (!Customer_Name) return res.status(400).json(error_missing_params('Customer_Name'));
+			if (!Location) return res.status(400).json(error_missing_params('Location'));
+			if (!Booking_Status) return res.status(400).json(error_missing_params('Booking_Status'));
+			if (!Payment_Status) return res.status(400).json(error_missing_params('Payment_Status'));
+			if (!Start_Date) return res.status(400).json(error_missing_params('Start_Date'));
+			if (!End_Date) return res.status(400).json(error_missing_params('End_Date'));
+			if (!Sub_Total) return res.status(400).json(error_missing_params('Sub_Total'));
+			if (!Discount) return res.status(400).json(error_missing_params('Discount'));
+			if (!Tip) return res.status(400).json(error_missing_params('Tip'));
+			if (!Total) return res.status(400).json(error_missing_params('Total'));
+			if (!Booking_Payable_to_Worker) return res.status(400).json(error_missing_params('Booking_Payable_to_Worker'));
+			if (!Worker_Information) return res.status(400).json(error_missing_params('Worker_Information'));
+
+			// const formData = new FormData();
+			// formData.append("Booking_ID", Booking_ID);
+			// formData.append("Customer_Name", Customer_Name);
+			// formData.append("Location", Location);
+			// formData.append("Booking_Status", Booking_Status);
+			// formData.append("Payment_Status", Payment_Status);
+			// formData.append("Start_Date", Start_Date);
+			// formData.append("End_Date", End_Date);
+			// formData.append("Sub_Total", Sub_Total);
+			// formData.append("Discount", Discount);
+			// formData.append("Tip", Tip);
+			// formData.append("Total", Total);
+			// formData.append("Booking_Payable_to_Worker", Booking_Payable_to_Worker);
+			// formData.append("Worker_Information", JSON.stringify(Worker_Information));
+
+			let data_send = {
+				Booking_ID,
+				Customer_Name,
+				Location,
+				Booking_Status,
+				Payment_Status,
+				Start_Date,
+				End_Date,
+				Sub_Total,
+				Discount,
+				Tip,
+				Total,
+				Booking_Payable_to_Worker,
+				Worker_Information: JSON.stringify(Worker_Information)
+			}
+
+			let environment = env === 'PRO' ? 'order-management' : 'om-sandbox';
+
+			let accessToken = await getRefreshToken()
+			.then((data) => Promise.resolve(data))
+			.catch((err) => Promise.reject(err));
+
+			const url = `${base_url}/${environment}/report/All_Bookings1/${App_Id_Booking}`;
+			console.log(url);
+			console.log(data_send)
+			const options = {
+			    method: 'PATCH',
+			    body: JSON.stringify({
+					data: {
+						...data_send,
+					},
+				}),
+			    headers: {
+					'Content-Type': 'application/json',
+			        Authorization: `Zoho-oauthtoken ${accessToken.access_token}`,
+			    },
+			};
+			const response = await fetch(url, options).catch(err => {return res.status(500).json({status: false, message: err})});
+			const data = await response.json();
+			if (!data) {
+				return res.status(500).json({
+					status: false,
+					data: []
+				})
+			};
+
+			return res.status(200).json({
+				data
+			});
+		} catch (err) {
+			next(err);
+		}
+	},
 };
 
 module.exports = CustomerFeatureController;
