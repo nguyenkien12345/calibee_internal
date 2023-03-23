79f1c26aefee05f332012711a8f445e2b21aa6fc
nhphu19 <nhphu19@gmail.com>
2023-03-23 23:30:10 +0700

    [phu] add some field

--- src/models/booking/Booking.js
+++ src/models/booking/Booking.js
@@ -3,222 +3,238 @@ const sequelize = require('../../config/database');
 const BookingDetail = require('../../models/booking/BookingDetail');
 
 const Booking = sequelize.define(
-	'booking',
-	{
-		booking_id: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-		customer_id: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		service_category_id: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		address: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		province_id: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		bulding_floor: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		gate: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		note: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		parent_type_id: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		have_pets: {
-			type: DataTypes.BOOLEAN,
-			allowNull: true,
-		},
-		ironing_clothes: {
-			type: DataTypes.BOOLEAN,
-			allowNull: true,
-		},
-		ironing_clothes_fee: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		favorite_worker: {
-			type: DataTypes.BOOLEAN,
-			allowNull: true,
-		},
-		premium: {
-			type: DataTypes.BOOLEAN,
-			allowNull: true,
-		},
-		days: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		start_day: {
-			type: DataTypes.DATEONLY,
-			allowNull: true,
-		},
-		start_time: {
-			type: DataTypes.TIME,
-			allowNull: true,
-		},
-		package: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		promotion_id: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		payment_method_id: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		tip: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		working_time: {
-			type: DataTypes.FLOAT.UNSIGNED,
-			allowNull: true,
-		},
-		total_time: {
-			type: DataTypes.FLOAT.UNSIGNED,
-			allowNull: true,
-		},
-		worker_earnings: {
-			type: DataTypes.FLOAT,
-			allowNull: true,
-		},
-		service_fee: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		total: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		lat: {
-			type: DataTypes.FLOAT,
-			allowNull: true,
-		},
-		long: {
-			type: DataTypes.FLOAT,
-			allowNull: true,
-		},
-		priority: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		priority_worker: {
-			type: DataTypes.STRING,
-			allowNull: true,
-		},
-		end_day: {
-			type: DataTypes.DATEONLY,
-			allowNull: true,
-		},
-		time_key: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		package_detail: {
-			type: DataTypes.STRING,
-			allowNull: true,
-		},
-		is_test: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-		app_id: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		booking_id_crm: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		momo_order_id: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		show_id: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		amount_worker: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			defaultValue: 1,
-		},
-		district: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		is_flag: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-		contract_id: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		own_tools: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-		foreign_language: {
-			type: DataTypes.STRING(100),
-			allowNull: true,
-		},
-		product_code_name: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		extra_service_fee_details: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		min_recomendation_max: {
-			type: DataTypes.TEXT,
-			allowNull: true,
-		},
-		origin_total_payment: {
-			type: DataTypes.INTEGER.UNSIGNED,
-			allowNull: true,
-		},
-		handwash: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: false,
-		},
-	},
-	{
-		paranoid: true,
-		charset: 'utf8mb4',
-		collate: 'utf8mb4_general_ci',
-	},
+    'booking',
+    {
+        booking_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        customer_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        service_category_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        address: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        province_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        bulding_floor: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        gate: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        note: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        parent_type_id: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        have_pets: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        ironing_clothes: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        ironing_clothes_fee: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        favorite_worker: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        premium: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        days: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        start_day: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+        },
+        start_time: {
+            type: DataTypes.TIME,
+            allowNull: true,
+        },
+        package: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        promotion_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        payment_method_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        tip: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        working_time: {
+            type: DataTypes.FLOAT.UNSIGNED,
+            allowNull: true,
+        },
+        total_time: {
+            type: DataTypes.FLOAT.UNSIGNED,
+            allowNull: true,
+        },
+        worker_earnings: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        service_fee: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        total: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        lat: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        long: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        priority: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        priority_worker: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        end_day: {
+            type: DataTypes.DATEONLY,
+            allowNull: true,
+        },
+        time_key: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        package_detail: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        is_test: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        app_id: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        booking_id_crm: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        momo_order_id: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        show_id: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        amount_worker: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            defaultValue: 1,
+        },
+        district: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        is_flag: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        contract_id: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        own_tools: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        foreign_language: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        product_code_name: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        extra_service_fee_details: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        min_recomendation_max: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        origin_total_payment: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: true,
+        },
+        handwash: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        commission_type: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        commission: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        commission_show: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        booking_changing: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+    },
+    {
+        paranoid: true,
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+    },
 );
 
 // Booking (1 -> 1) Booking Detail
 Booking.hasOne(BookingDetail, {
-	as: 'booking_detail',
-	foreignKey: 'booking_id',
+    as: 'booking_detail',
+    foreignKey: 'booking_id',
 });
 
 BookingDetail.belongsTo(Booking, {
-	as: 'booking',
-	foreignKey: 'booking_id',
+    as: 'booking',
+    foreignKey: 'booking_id',
 });
 
 module.exports = Booking;
